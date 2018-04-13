c9c2f3bd110bc3c9cfcbd52150badfd03fc731a2
cjh <1418503647@qq.com>
2018-04-14 01:10:35 +0800

    backup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
     <my-page :title="title" :page="page">
-        <ui-raised-button class="file-btn" label="打开 TXT">
+        <ui-raised-button class="file-btn" label="打开 TXT" v-if="!content">
             <input type="file" class="ui-file-button" @change="fileChange($event, 1)">
         </ui-raised-button>
-        <pre>{{ content }}</pre>
+        <pre class="content" :style="contentStyle">{{ content }}</pre>
+        <ui-drawer right :open="open" :docked="false" @close="toggle()">
+            <ui-appbar title="设置">
+                <ui-icon-button icon="close" @click="toggle" slot="left" />
+            </ui-appbar>
+            <div class="body">
+                <ui-text-field type="number" v-model.number="style.fontSize" />
+            </div>
+        </ui-drawer>
     </my-page>
 </template>
 
 <script>
+    const IntentEx = window.IntentEx
+
     export default {
         data () {
             return {
                 title: '文本阅读器',
                 content: '',
+                open: false,
+                style: {
+                    fontSize: 20
+                },
                 page: {
                     menu: [
                         {
@@ -31,8 +45,38 @@
                 }
             }
         },
+        computed: {
+            contentStyle() {
+                return {
+                    'font-size': this.style.fontSize + 'px'
+                }
+            }
+        },
+        mounted() {
+            let text = this.$route.query.text
+            if (text) {
+                this.content = text
+            }
+            
+            let intent = new IntentEx(data => {
+                this.content = data
+            }, error => {
+                console.log('不支持啊')
+            })
+
+            // document.getElementById('send').addEventListener("click", function() {
+            //     console.log('editor send message')
+            //     let data = document.getElementById('data').value
+            //     intent.sendResponse(data)
+            // }, false);
+        },
         methods: {
-            setting() {},
+            setting() {
+                this.toggle()
+            },
+            toggle () {
+                this.open = !this.open
+            },
             fileChange(e) {
                 let file = e.target.files[0]
                 this.title = file.name
@@ -61,5 +105,13 @@
     top: 0;
     bottom: 0;
     opacity: 0;
+    margin-bottom: 16px;
+}
+.content {
+    font-size: 16px;
+    font-family: Helvetica Neue,Helvetica,PingFang SC,Hiragino Sans GB,Microsoft YaHei,微软雅黑,Arial,sans-serif;
+}
+.body {
+    padding: 16px;
 }
 </style>
