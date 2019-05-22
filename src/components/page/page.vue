ffb33b1a2f7d77c4d499c557cc2618e2ee68ea59
yunser <>
2019-05-22 22:24:04 +0800

    backup

--- src/components/page/page.vue
+++ src/components/page/page.vue
@@ -6,7 +6,7 @@
                 <ui-list-item title="首页" to="/">
                     <ui-icon slot="left" value="grade"/>
                 </ui-list-item>
-                <ui-list-item title="关于" to="/about">
+                <ui-list-item title="关于" href="https://project.yunser.com/products/f20266305c5111e888a50737631f72f2" target="_blank">
                     <ui-icon slot="left" value="grade"/>
                 </ui-list-item>
             </ui-list>
